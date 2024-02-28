cc882b068ef3138d47359b8f49760faa8d5dd4b0
raphael taguemkam <raphaeltaguemkam@gmail.com>
2024-02-27 21:25:24 -0700

    fixed issue as per checkov recommendations

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,11 @@
 resource "aws_instance" "web" {
   ami           = var.AMI_ID
   instance_type = var.INSTANCE_TYPE
-
+  metadata_options {
+     http_endpoint = "enabled"
+     http_tokens   = "required"
+  }
+  iam_instance_profile = "AdminRoleEC2"
   tags = {
     Name = "airbnb-web-server"
   }
